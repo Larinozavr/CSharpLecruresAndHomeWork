57dde7f3375ce05d7fb0fd1bee4697b005367d54
Maxon Zavulon <larinozavr@gmail.com>
2022-12-01 20:31:39 +0300

    Replace Task1

---
+++ HomeWork1/Task1/FindMaxFrom2Digits.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Input first number 'a':");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input second number 'b':");
+int b = Convert.ToInt32(Console.ReadLine());
+if(a > b)
+{
+	int max = a;
+	Console.WriteLine("max = {0}",max);
+}
+else
+{
+	int max = b;
+	Console.WriteLine("max = {0}",max);
+}	
