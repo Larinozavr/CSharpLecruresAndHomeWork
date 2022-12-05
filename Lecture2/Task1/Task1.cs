aab9276fe4d780e68af60bfa675dd08912e91e8a
Maxon Zavulon <larinozavr@gmail.com>
2022-12-05 20:04:43 +0300

    Add Lecrure 2 with task1

---
+++ Lecture2/Task1/Task1.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите число 'a':");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 'b':");
+int b = Convert.ToInt32(Console.ReadLine());
+if(a < b)
+{    
+    if(b % a == 0)
+        Console.WriteLine($"Второе число кратно первому");
+    else
+    {
+         Console.WriteLine($"Число не кратно, остаток - {b%a}");  
+    }
+}
+else
+{
+    if(a % b == 0)
+        Console.WriteLine($"Второе число кратно первому");
+    else
+    {
+         Console.WriteLine($"Число не кратно, остаток - {a%b}");  
+    }
+}
