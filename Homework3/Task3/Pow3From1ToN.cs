d6e0e9dabc8bef083496556ae83c25b184b39d33
Maxon Zavulon <larinozavr@gmail.com>
2022-12-08 21:08:12 +0300

    Added task 3

---
+++ Homework3/Task3/Pow3From1ToN.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Please, input number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] arrayOfCubes = new int[n];
+for (int i = 1;  i < n + 1; i++ )
+{
+    arrayOfCubes[i-1] = Convert.ToInt32(Math.Pow(i,3));
+}
+void ArrayPrinter(int[] a)
+{
+    Console.Write("This is your array of cubes: ");
+    foreach (int i in arrayOfCubes)
+    {
+        Console.Write($"[{i}] ");
+    }
+}    
+ArrayPrinter(arrayOfCubes);
