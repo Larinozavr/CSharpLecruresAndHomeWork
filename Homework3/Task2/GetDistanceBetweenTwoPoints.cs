1f08f57ababda0835e346b4fc20497381edd6918
Maxon Zavulon <larinozavr@gmail.com>
2022-12-08 20:21:16 +0300

    Added task 2

---
+++ Homework3/Task2/GetDistanceBetweenTwoPoints.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Please, input X for point A:");
+double X1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Please, input Y for point A: ");
+double Y1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Please, input Z for point A: ");
+double Z1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Please, input X for point B: ");
+double X2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Please, input Y for point B: ");
+double Y2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Please, input Z for point B: ");
+double Z2 = Convert.ToDouble(Console.ReadLine());
+double DistanceBetweenTwoPoints = Math.Sqrt(Math.Pow((X2 - X1),2) + Math.Pow((Y2 - Y1),2) + Math.Pow((Z2 - Z1),2));
+Console.WriteLine($"The distanse is: {DistanceBetweenTwoPoints}");
